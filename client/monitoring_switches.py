f8643ad819960745039ae968846dd4792ea3cc8f
kovalew.vladimir <kovalew.vladimir@gmail.com>
2018-06-28 16:58:45 +0300

    Оптимизировал код

--- client/monitoring_switches.py
+++ client/monitoring_switches.py
@@ -1,9 +1,11 @@
 import json
+from termcolor import colored
 import MySQLdb
 
 
 class MonitoringSwitches:
     
+
     def __init__(self, cred_db):
         
         self.cred_db  = cred_db
@@ -11,6 +13,17 @@ class MonitoringSwitches:
 
         db = MySQLdb.connect(**self.cred_db)
         cursor = db.cursor()
+
+        self._db_switches_port(cursor)
+        self._db_switches_mac(cursor)
+
+        db.close()
+
+        self.all_mac_switches = self._get_all_mac_switches()
+
+
+    def _db_switches_port(self, cursor):
+        
         query_all_switches = '''
             SELECT switches.id_switches, switches.ip, ports.port_number, FDB_tables.mac_address
             FROM switches
@@ -26,43 +39,160 @@ class MonitoringSwitches:
             id_switches, ip, port_number, mac = switch
 
             if self.switches.get(ip) is None:
-                self.switches[ip] = {}
+                self.switches[ip]          = {}
                 self.switches[ip]['ports'] = {}
             if self.switches[ip]['ports'].get(port_number) is None:
-                self.switches[ip]['ports'][port_number] = set()
+                self.switches[ip]['ports'][port_number]               = {}
+                self.switches[ip]['ports'][port_number]['init_mac']   = set()
+                self.switches[ip]['ports'][port_number]['change_mac'] = set()
             
-            self.switches[ip]['ports'][port_number].add(mac)
+            self.switches[ip]['ports'][port_number]['init_mac'].add(mac.upper())
+            self.switches[ip]['ports'][port_number]['change_mac'].add(mac.upper())
             self.switches[ip]['id_switches'] = id_switches
 
-        db.close()
 
-    def _foreach_switches(self, func_handler):
+    def _db_switches_mac(self, cursor):
+        query_all_switches = '''
+            SELECT switches.id_switches, switches.ip, statistics_ports.port_mac
+            FROM statistics_ports
+            INNER JOIN ports using(id_ports)
+            INNER JOIN switches using(id_switches)
+            INNER JOIN requests using(id_requests)
+            WHERE requests.DATE = (SELECT MAX(requests.DATE) FROM requests);
+        '''
+        cursor.execute(query_all_switches)
+        data =  cursor.fetchall()
+
+        for switch in data:
+            id_switches, ip, mac = switch
+            if self.switches.get(ip) is None:
+                self.switches[ip] = {}
+                self.switches[ip]['id_switches'] = id_switches
+                print(colored("ERROR: нет портов у %s"%(ip), 'red'))
+            if self.switches[ip].get('mac') is None:
+                self.switches[ip]['mac'] = set()
+            self.switches[ip]['mac'].add(mac.upper())
+
+
+    def _foreach_switches(self, func_handler_port, func_handler_switch=None):
         for ip_switch in self.switches.keys():
-            id_switches = self.switches[ip_switch]['id_switches']
-            for port in self.switches[ip_switch]['ports'].keys():
-                mac = self.switches[ip_switch]['ports'][port]
-                func_handler(id_switches=id_switches, port=port, mac=mac)
+            id_switches = self.switches[ip_switch].get('id_switches')
+            mac_swithces = self.switches[ip_switch].get('mac')
+
+            if func_handler_switch is not None:
+                func_handler_switch(id_switches=id_switches, ip_switch= ip_switch, mac_swithces=mac_swithces)
+                continue
+
+            ports = self.switches[ip_switch].get('ports')
+            if ports is None:
+                continue
+            for port in ports.keys():
+                init_mac   = self.switches[ip_switch]['ports'][port]['init_mac']
+                change_mac = self.switches[ip_switch]['ports'][port]['change_mac']
+                func_handler_port(
+                    id_switches=id_switches, 
+                    ip_switch= ip_switch, 
+                    mac_swithces=mac_swithces, 
+                    port=port, 
+                    init_mac=init_mac, 
+                    change_mac=change_mac)
+
+
+    def _get_all_mac_switches(self):
+        def _handler(**kwargs):
+            mac_swithces = kwargs['mac_swithces']
+            all_mac_switches.update(mac_swithces)
+        
+        all_mac_switches = set()
+        self._foreach_switches(None, _handler)
+        return all_mac_switches
+
 
     def get_final_mac(self):
         def _handler(**kwargs):
-            mac = kwargs['mac']
-            if len(mac) == 1:
-                final_mac.update(mac)
+            change_mac = kwargs['change_mac']
+            if len(change_mac) == 1:
+                final_mac.update(change_mac)
         
         final_mac = set()
         self._foreach_switches(_handler)
+
+        #final_mac.difference_update(self.all_mac_switches)
         return final_mac
+
+    
+    def clear_all_mac_switches_of_ports(self):
+        def _handler(**kwargs):
+            change_mac = kwargs['change_mac']
+            if len(change_mac) > 1:
+                change_mac.difference_update(self.all_mac_switches)
+        
+        self._foreach_switches(_handler)
+
 
     def clear_final_mac_of_ports(self):
         def _handler(**kwargs):
-            mac = kwargs['mac']
-            if len(mac) > 1:
-                mac.difference_update(final_mac)
+            change_mac = kwargs['change_mac']
+            if len(change_mac) > 1:
+                change_mac.difference_update(final_mac)
 
         final_mac = self.get_final_mac()
         self._foreach_switches(_handler)
-        
 
+
+    def print_count(self):
+        def _handler(**kwargs):
+            ip_switch    = kwargs['ip_switch']
+            port         = kwargs['port']
+            mac_swithces = kwargs['mac_swithces']
+            init_mac     = kwargs['init_mac']
+            change_mac   = kwargs['change_mac']
+            print('ip: %15s, port: %3d, count mac_swithces: %3d, count init_mac: %4d, count change_mac: %4d' % (
+                ip_switch, port, len(mac_swithces), len(init_mac), len(change_mac)))
+         
+        self._foreach_switches(_handler)
+
+
+    def get_tree_switch_nodes(self):
+        def _handler(**kwargs):
+            id_switches  = kwargs['id_switches']
+            ip_switch    = kwargs['ip_switch']
+            
+            nodes.append({
+                'id'   : id_switches,
+                'label': ip_switch,
+            })
+
+
+        nodes = []
+        self._foreach_switches(None, _handler)
+        return json.dumps(nodes)
+
+    def get_tree_switch_edges(self):
+        def _handler(**kwargs):
+            def _handler_port(**kwargs):
+                id_switches = kwargs['id_switches']
+                ip_switch   = kwargs['ip_switch']
+                port        = kwargs['port']
+                change_mac  = kwargs['change_mac']
+                if (_id_switches == id_switches):
+                    return
+                if len(change_mac & _mac_swithces) != 0:
+                    edges.append({
+                        'from'  : _id_switches,
+                        'to'    : id_switches,
+                        'title' : '%s-%s(%d)'%(_ip_switch, ip_switch, port),
+                    })
+
+            _id_switches  = kwargs['id_switches']
+            _ip_switch    = kwargs['ip_switch']
+            _mac_swithces = kwargs['mac_swithces']
+            
+            self._foreach_switches(_handler_port)
+        edges = []
+        self._foreach_switches(None, _handler)
+        return json.dumps(edges)
+        
 
 if __name__ == "__main__":
 
@@ -75,7 +205,13 @@ if __name__ == "__main__":
     }
 
     switch = MonitoringSwitches(cred)
-    print(len(switch.get_final_mac()))
+    
+    switch.clear_final_mac_of_ports()
+    
+    print(switch.get_tree_switch_nodes())
+    print(switch.get_tree_switch_edges())
+
+    #switch.print_count()
 
 
 def tree_swicthes(switches):
