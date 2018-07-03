f337476ecb6a6efb84c59e2685381d3085a633ba
kovalew.vladimir <kovalew.vladimir@gmail.com>
2018-07-03 12:43:56 +0300

    Добавил flask

---
+++ client/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+from monitoring_switches import MonitoringSwitches
+
+app = Flask(__name__)
+
+cred = {
+        'host'    : '10.4.5.54',
+        'user'    : 'pysnmp',
+        'passwd'  : '123456',
+        'db'      : 'switch_snmp',
+        'charset' : 'utf8',
+    }
+
+    
+
+@app.route('/')
+def maps():
+    switchs = MonitoringSwitches(cred)
+
+    nodes = switchs.get_tree_switch_nodes()
+    edges = switchs.get_tree_switch_edges()
+
+    return render_template('maps.html', nodes=nodes, edges=edges)
