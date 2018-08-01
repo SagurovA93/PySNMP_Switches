ebe7130a28492f092ead76423cae7a872f90edec
Sagurov Anton <anton.sagurov@gmail.com>
2018-08-01 23:21:48 +0300

    Исправлена ошибка: пустой max(id_requests)
    Ошибка из-за того что max(id_requests) брался из другой таблицы, нежели
    первая...

--- server/SQL/LLDP_table_using_id_sw&id_rqst.sql
+++ server/SQL/LLDP_table_using_id_sw&id_rqst.sql
@@ -7,7 +7,7 @@ select * from
                 and id_requests = (select max(id_requests) from (
 					select * from 
 						ports inner join 
-						statistics_ports using(id_ports) inner join 
+						LLDP_table using(id_ports) inner join 
 						requests using(id_requests) 
 						WHERE id_switches = '1' ) 
                         as tmp
--- server/SQL/statistics_switch_using_id_sw&id_rqst.sql
+++ server/SQL/statistics_switch_using_id_sw&id_rqst.sql
@@ -4,5 +4,4 @@ inner join requests using(id_requests)
 	where id_switches = '19' 
     and id_requests = (select max(id_requests) from (
 		select id_requests from statistics_switch
-		WHERE id_switches = '19' ) as tmp
-	);
+		WHERE id_switches = '19' ) as tmp );
